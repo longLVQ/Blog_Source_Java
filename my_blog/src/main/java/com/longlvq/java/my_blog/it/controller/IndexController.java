1bf957199adba6f452dab19769e271d51ca83c16
longLVQ <levanquoclong.9a10@gmail.com>
2020-06-15 23:04:04 +0700

    ewq

--- my_blog/src/main/java/com/longlvq/java/my_blog/it/controller/IndexController.java
+++ my_blog/src/main/java/com/longlvq/java/my_blog/it/controller/IndexController.java
@@ -1,12 +1,22 @@
 package com.longlvq.java.my_blog.it.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.longlvq.java.my_blog.it.entity.UserEntity;
+import com.longlvq.java.my_blog.it.service.UserService;
 
 @RestController
 public class IndexController {
-	@RequestMapping("/")
-	public String index() {
-		return "Hành à. mi ngu quá";
+	@Autowired
+	UserService userService;
+	
+	@RequestMapping(value = "/",method = RequestMethod.GET) 
+	public ResponseEntity<UserEntity> index() {
+		return new ResponseEntity<UserEntity>(userService.getUserById(1), HttpStatus.OK);
 	}
 }
